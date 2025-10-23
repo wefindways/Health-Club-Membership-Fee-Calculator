f2441436da3b318a029b816cf6e51c235a3e2bec
Mac <marcjosephcruz24@gmail.com>
2025-10-23 11:21:25 +0800

    fix: resolve css imports for both files (#8)

--- process-order.php
+++ process-order.php
@@ -54,7 +54,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Order Summary - Membership Fee Calculator</title>
-  <link rel="stylesheet" href="process.css">
+  <link rel="stylesheet" href="css/process-order.css">
 </head>
 <body>
   <?php 
